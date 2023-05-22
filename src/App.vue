d1690cb1e5ac0d293a4b25d8da6b662bc318700b
Jalen Heidelburg <rwxjalen@gmail.com>
2023-05-21 22:21:14 -0400

    starting base of project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+    <hello-world msg="You did it again!" />
+  </div>
+</template>
