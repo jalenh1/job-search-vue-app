70d57820726763465aeddc7ea01ac788c9f5d78b
Jalen Heidelburg <rwxjalen@gmail.com>
2023-05-23 21:53:20 -0400

    write a test for testing rending main navigation items

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,18 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Honey Jobs')
     expect(companyName).toBeInTheDocument()
   })
+
+  it('displays menu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Honey Jobs',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 })
