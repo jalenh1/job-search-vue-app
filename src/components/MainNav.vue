4e2e2cd138c0e72b4437fc573f5ce435bbfd2262
jalen heidelburg <rwxjalen@gmail.com>
2023-05-26 10:59:10 -0400

    get profile image setup and get the basic login function working

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -11,7 +11,8 @@
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <action-button />
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else @click="loginUser" />
         </div>
       </div>
     </div>
@@ -20,17 +21,25 @@
 
 <script>
 import ActionButton from '@/components/ActionButton.vue'
+import ProfileImage from '@/components/ProfileImage.vue'
 
 export default {
   name: 'MainNav',
   components: {
-    ActionButton
+    ActionButton,
+    ProfileImage
   },
   data() {
     return {
       company: 'Honey Jobs',
       url: 'https://careers.google.com',
-      menuItems: ['Teams', 'Locations', 'Life at Honey Jobs', 'How we hire', 'Students', 'Jobs']
+      menuItems: ['Teams', 'Locations', 'Life at Honey Jobs', 'How we hire', 'Students', 'Jobs'],
+      isLoggedIn: false
+    }
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true
     }
   }
 }
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageLink" alt="profile image" class="h-8 w-8 rounded-3xl object-contain" />
+</template>
+
+<script>
+export default {
+  name: 'ProfileImage',
+  data() {
+    return {
+      imageLink:
+        'https://e7.pngegg.com/pngimages/739/902/png-clipart-super-mario-illustration-super-mario-bros-nintendo-badge-arcade-super-mario-rpg-luigi-super-mario-bros-nintendo.png'
+    }
+  }
+}
+</script>
