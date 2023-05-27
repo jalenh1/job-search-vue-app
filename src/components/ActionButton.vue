32b1007159fb443e8442b845ca2eed9578658e0c
Jalen Heidelburg <rwxjalen@gmail.com>
2023-05-26 22:22:22 -0400

    passing props to configure action button

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,30 @@
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Sign in
+  <button :class="{ primary: primary }">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
-  name: 'ActionButton'
+  name: 'ActionButton',
+  props: ['text'],
+  data() {
+    return {
+      primary: true
+    }
+  }
 }
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+}
+</style>
