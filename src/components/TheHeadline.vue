66958a303eb326090d2188b7185a11cd119622ac
jalen heidelburg <rwxjalen@gmail.com>
2023-05-30 10:59:47 -0400

    move verb array logic to utils folder

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,7 +1,11 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at Honey Jobs</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Honey Jobs</h2>
   </section>
 </template>
 
@@ -12,6 +16,13 @@ export default {
     return {
       action: 'Build',
       interval: null
+    }
+  },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLowerCase()]: true
+      }
     }
   },
   created() {
@@ -33,3 +44,21 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
