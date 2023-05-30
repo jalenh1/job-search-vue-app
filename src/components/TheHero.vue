076937889facaa89c9693775488257b68b15decc
Jalen Heidelburg <rwxjalen@gmail.com>
2023-05-29 21:29:16 -0400

    style hero section and work on setting up the grid layout

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    <section>This is the hero</section>
+    <section class="flex h-screen flex-col bg-green-300 pb-20 pt-10">
+      <div class="grid-col-12 grid">
+        <div class="col-span-1 col-start-1 border border-blue-800">1 column</div>
+        <div class="col-span-5 col-start-2 border border-blue-700">5 column</div>
+        <div class="col-span-5 col-start-7 border border-blue-700">5 column</div>
+        <div class="col-span-1 col-start-12 border border-blue-700">1 column</div>
+      </div>
+    </section>
   </main>
 </template>
 
