f14f8e6888724c32e6e4095016ba4f9163a745de
jalen heidelburg <rwxjalen@gmail.com>
2023-05-31 12:57:00 -0400

    added vue router to the project and refactored some code to inclue router-links

---
+++ src/views/JobResultsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="text-6xl font-bold">Job Results Page</h1>
+  <button @click="handleClick">Go to Home Page</button>
+</template>
+
+<script>
+export default {
+  name: 'JobResultsView',
+  methods: {
+    handleClick() {
+      this.$router.push({
+        name: 'Home'
+      })
+    }
+  }
+}
+</script>
