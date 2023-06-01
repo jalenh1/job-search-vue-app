7ef6502ce5409aeb93d003cb1ba43f0cda7ea8b4
Jalen Heidelburg <rwxjalen@gmail.com>
2023-05-31 21:22:51 -0400

    created components for job listings and more styling

---
+++ src/components/JobResults/JobListing.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="mb-7">
+    <router-link
+      to="/jobs/results/1"
+      class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+    >
+      <div class="mx-8 border-b border-solid border-brand-gray-2 pb-2 pt-5">
+        <h2 class="tex-2xl mb-2">Vue Developer</h2>
+        <div class="flex flex-row align-middle">
+          <div class="mr-5">
+            <span>Honey</span>
+          </div>
+          <div>
+            <span>Toledo, OH, USA</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4">
+        <div>
+          <h3 class="mb-2 mt-1">Qualifications:</h3>
+          <div>
+            <ul class="list-disc pl-8">
+              <li>JavaScript</li>
+              <li>Vue</li>
+              <li>Nuxt</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="mt-2 text-center">
+          <router-link to="/jobs/results/1" class="text-brand-blue-1"> Expand</router-link>
+        </div>
+      </div>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'JobListing'
+}
+</script>
