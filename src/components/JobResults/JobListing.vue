05787c4012c13e7b1bd888b6704ad7bbafbdef5e
jalen heidelburg <rwxjalen@gmail.com>
2023-06-01 10:31:39 -0400

    added fetching job data and rendering it on the view

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,21 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pb-2 pt-5">
-        <h2 class="tex-2xl mb-2">Vue Developer</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Honey</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>Toledo, OH, USA</span>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="mr-5 inline-block">
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -21,15 +25,15 @@
           <h3 class="mb-2 mt-1">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>JavaScript</li>
-              <li>Vue</li>
-              <li>Nuxt</li>
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                {{ qualification }}
+              </li>
             </ul>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"> Expand</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1"> Expand</router-link>
         </div>
       </div>
     </router-link>
@@ -38,6 +42,17 @@
 
 <script>
 export default {
-  name: 'JobListing'
+  name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`
+    }
+  }
 }
 </script>
--- src/playground.js
+++ src/playground.js
@@ -2,6 +2,13 @@ const axios = require('axios')
 
 const url = 'http://localhost:3000/jobs'
 
-axios.get(url).then((response) => {
+const fetchJobsv1 = () => {
+  axios.get(url).then((response) => {
+    console.log(response.data)
+  })
+}
+
+const fetchJobsv2 = async () => {
+  const response = await axios.get(url)
   console.log(response.data)
-})
+}
