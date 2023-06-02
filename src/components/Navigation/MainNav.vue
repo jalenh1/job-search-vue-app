83040c578f1fc79e0360863850d0fae4eb16e78e
jalen heidelburg <rwxjalen@gmail.com>
2023-06-02 11:10:06 -0400

    created a teams page for the site

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -41,7 +41,7 @@ export default {
   data() {
     return {
       menuItems: [
-        { text: 'Teams', url: '/' },
+        { text: 'Teams', url: '/teams' },
         { text: 'Locations', url: '/' },
         { text: 'Life at Honey Jobs', url: '/' },
         { text: 'How we hire', url: '/' },
