ee02f2451d33d411369f239158057d155b3d25ce
jalen heidelburg <rwxjalen@gmail.com>
2023-06-02 10:50:34 -0400

    working on side bar filtering feature on jobs page

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  }
+}
+</script>
