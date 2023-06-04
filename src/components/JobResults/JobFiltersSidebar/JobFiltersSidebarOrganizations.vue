306e1c42bde2be77f06444195c62ce3d55c498f4
Jalen Heidelburg <rwxjalen@gmail.com>
2023-06-04 09:44:23 -0400

    setting up state in pinia store

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -3,21 +3,16 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube">VueTube</label>
+          <li v-for="organization in UNIQUE_ORGANIZATIONS" :key="organization" class="h-8 w-1/2">
+            <input
+              :id="organization"
+              v-model="selectedOrganizations"
+              :value="organization"
+              type="checkbox"
+              class="mr-3"
+              @change="selectOrganization"
+            />
+            <label :for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
@@ -26,12 +21,27 @@
 </template>
 
 <script>
+import { mapState } from 'pinia'
+import { useJobsStore, UNIQUE_ORGANIZATIONS } from '@/stores/jobs'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
 
 export default {
   name: 'JobFiltersSidebarOrganizations',
   components: {
     CollapsibleAccordion
+  },
+  data() {
+    return {
+      selectedOrganizations: []
+    }
+  },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS])
+  },
+  methods: {
+    selectOrganization() {
+      console.log(this.selectedOrganizations)
+    }
   }
 }
 </script>
